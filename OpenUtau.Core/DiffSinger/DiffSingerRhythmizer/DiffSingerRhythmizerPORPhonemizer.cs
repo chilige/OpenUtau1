18afaa4d639a0c35fba12eab92c8460736c2260d
Atony Xu <31569794+atonyxu@users.noreply.github.com>
2023-11-15 18:00:04 +0800

    add rus and por phonemizer

---
+++ OpenUtau.Core/DiffSinger/DiffSingerRhythmizer/DiffSingerRhythmizerPORPhonemizer.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenUtau.Api;
+using OpenUtau.Core.G2p;
+using OpenUtau.Core.Ustx;
+
+namespace OpenUtau.Core.DiffSinger {
+
+    [Phonemizer("DiffSinger Portuguese RHY Phonemizer", "DIFFS POR-RHY", "BaiTang", language: "DIFFS-RHY")]
+    public class DiffSingerRhythmizerPORPhonemizer : DiffSingerRhythmizerG2pPhonemizer {
+        public override void SetUpPhoneDictAndRhy() {
+            LoadSingerRhythmizer("POR");
+            this.g2p = LoadG2p();
+            GetRealPhnDict("ds_POR.txt");
+            GetRhyMap();
+        }
+
+        protected override void ProcessPart(Note[][] phrase) {
+            ProcessByRhyMap(phrase);
+        }
+
+        protected override string GetDictionaryName()=>"dsdict-pt.yaml";
+        protected override IG2p LoadBaseG2p() => new PortugueseG2p();
+        protected override string[] GetBaseG2pVowels() => new string[] {
+            "E", "O", "a", "a~", "e", "e~", "i", "i~", "o", "o~", "u", "u~"
+        };
+
+        protected override string[] GetBaseG2pConsonants() => new string[] {
+            "J", "L", "R", "S", "X", "Z", "b", "d", "dZ", "f", "g", "j", "j~", 
+            "k", "l", "m", "n", "p", "r", "s", "t", "tS", "v", "w", "w~", "z"
+        };
+    }
+}
---
+++ OpenUtau.Core/DiffSinger/DiffSingerRhythmizer/DiffSingerRhythmizerRUSPhonemizer.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenUtau.Api;
+using OpenUtau.Core.G2p;
+using OpenUtau.Core.Ustx;
+
+namespace OpenUtau.Core.DiffSinger {
+
+    [Phonemizer("DiffSinger Russian RHY Phonemizer", "DIFFS RUS-RHY", "BaiTang", language: "DIFFS-RHY")]
+    public class DiffSingerRhythmizerRUSPhonemizer : DiffSingerRhythmizerG2pPhonemizer {
+        public override void SetUpPhoneDictAndRhy() {
+            LoadSingerRhythmizer("RUS");
+            this.g2p = LoadG2p();
+            GetRealPhnDict("ds_RUS.txt");
+            GetRhyMap();
+        }
+
+        protected override void ProcessPart(Note[][] phrase) {
+            ProcessByRhyMap(phrase);
+        }
+
+        protected override string GetDictionaryName()=>"dsdict-ru.yaml";
+        protected override IG2p LoadBaseG2p() => new RussianG2p();
+        protected override string[] GetBaseG2pVowels() => new string[] {
+            "a", "aa", "ay", "ee", "i", "ii", "ja", "je", "jo", "ju", "oo",
+            "u", "uj", "uu", "y", "yy"
+        };
+
+        protected override string[] GetBaseG2pConsonants() => new string[] {
+            "b", "bb", "c", "ch", "d", "dd", "f", "ff", "g", "gg", "h", "hh",
+            "j", "k", "kk", "l", "ll", "m", "mm", "n", "nn", "p", "pp", "r", 
+            "rr", "s", "sch", "sh", "ss", "t", "tt", "v", "vv", "z", "zh", "zz"
+        };
+    }
+}
